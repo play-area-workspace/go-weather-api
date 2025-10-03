db00451e3c4c6efb07b4edf7a33528db9619a485
sashmitha-atigala-codimite <sashmitha.atigala@codimite.com>
2025-10-03 16:42:38 +0530

    Initial commit with weather fetching functionality

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/play-area-workspace/go-weather-api
+
+go 1.24.6
+
+require github.com/joho/godotenv v1.5.1
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+)
+
+type Temperature struct {
+	Temp float64 `json:"temp"`
+}
+
+type WeatherResponse struct {
+	Main Temperature `json:"main"`
+}
+
+func fetchWeatherData(lat, lon, apiKey string, logger *slog.Logger) (*Temperature, error) {
+
+	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&appid=%s", lat, lon, apiKey)
+
+	weatherResponse := WeatherResponse{}
+	resp, err := http.Get(url)
+	if err != nil {
+		logger.Error("Error making HTTP request", "error", err.Error())
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+
+	if err := json.NewDecoder(resp.Body).Decode(&weatherResponse); err != nil {
+		logger.Error("Error decoding JSON response", "error", err.Error())
+		return nil, err
+	}
+	return &weatherResponse.Main, nil
+}
+
+func main() {
+
+	startNow := time.Now()
+	defer func() {
+		fmt.Printf("Execution time: %s\n", time.Since(startNow))
+	}()
+
+	var logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	if err := godotenv.Load(); err != nil {
+		logger.Error("Error loading .env file", "error", err.Error())
+	} else {
+		logger.Info(".env file loaded successfully")
+	}
+
+	logger.Info("Application started")
+	defer logger.Info("Application stopped")
+
+	apiKey := os.Getenv("API_KEY")
+
+	cities := make(map[string][2]string)
+	// Latitude, Longitude
+	cities["Colombo"] = [2]string{"6.9271", "79.8612"}
+	cities["New York"] = [2]string{"40.7128", "-74.0060"}
+	cities["London"] = [2]string{"51.5074", "-0.1278"}
+	cities["Tokyo"] = [2]string{"35.6895", "139.6917"}
+
+	for _, city := range cities {
+		temperature, err := fetchWeatherData(city[0], city[1], apiKey, logger)
+		if err != nil {
+			logger.Error("Failed to fetch weather data", "error", err.Error(), "city", city)
+			continue
+		}
+		logger.Info("Fetched weather data successfully", "city", city, "temperature(K)", temperature.Temp)
+	}
+}
